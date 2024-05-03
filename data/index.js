d30865fe6fba904ade817b37d6ede857169fde3e
zhuo zhang <zzhan114@stevens.edu>
2024-05-03 14:31:02 -0400

    updated data/index.js

--- data/index.js
+++ data/index.js
@@ -1,15 +1,10 @@
-import * as commentsFunctions from './comments.js';
-import * as productsFunctions from './products.js';
-import * as reviewsforproductsFunctions from './reviewsforproducts.js';
-import * as reviewsforstoresFunctions from './reviewsforstores.js';
-import * as storesFunctions from './stores.js';
-import * as usersFunctions from './users.js';
-import * as commentsforstoresFunctions from './commentsforstores.js';
 
-export const commentsData = commentsFunctions;
-export const productsData = productsFunctions;
-export const reviewsforproductsData = reviewsforproductsFunctions;
-export const reviewsforstoresData = reviewsforstoresFunctions;
-export const storesData = storesFunctions;
+import * as reviewsforhousingFunctions from './reviewsforstores.js';
+import * as housingFunctions from './stores.js';
+import * as usersFunctions from './users.js';
+import * as commentsforreviewsFunctions from './commentsforstores.js';
+
+export const reviewsforhousingData = reviewsforhousingFunctions;
+export const housingData = housingFunctions;
 export const usersData = usersFunctions;
-export const commentsforstoresData = commentsforstoresFunctions;
+export const commentsforreviewsData = commentsforreviewsFunctions;
