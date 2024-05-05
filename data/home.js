85573c3e3de74bdec1defe42b2a5b47d3d0ecc0e
zhangchongyuan <zhangchongyuan0811@163.com>
2024-05-04 22:16:37 -0400

    fix bug
    fix the bug that can not show the search results

--- data/home.js
+++ data/home.js
@@ -3,34 +3,39 @@ import helpers from "../helpers.js";
 
 export async function getHousingSearchResults(searchTerm, filters = {}) {
     searchTerm = helpers.checkSearchValid(searchTerm);
-
     const housingsCollection = await housing_collection();
-    const regex = new RegExp(searchTerm, 'i');
 
     // Build the query based on searchTerm and filters
-    const query = {
-        $or: [
-            { address: regex },
-            { city: regex },
-            { state: regex },
-            { zipCode: regex },
-            { amenities: regex }
-        ]
-    };
+    const query = {};
 
+    // 使用正则表达式构建通用的搜索条件，匹配多个字段
+    const regex = new RegExp(searchTerm, 'i');
+    query.$or = [
+        { address: regex },
+        { city: regex },
+        { state: regex },
+        { zipCode: regex },
+        { amenities: regex }
+    ];
+
+    // 将其他过滤条件添加到查询中
     if (filters.homeType) {
         query.homeType = filters.homeType;
     }
-    if (filters.rentalCostMin !== undefined) {
-        query.rentalCostMin = { $gte: filters.rentalCostMin };
-    }
-    if (filters.rentalCostMax !== undefined) {
-        query.rentalCostMax = { ...query.rentalCostMin, $lte: filters.rentalCostMax };
+    if (filters.rentalCostMin !== undefined || filters.rentalCostMax !== undefined) {
+        query.rentalCost = {};
+        if (filters.rentalCostMin !== undefined) {
+            query.rentalCost.$gte = filters.rentalCostMin;
+        }
+        if (filters.rentalCostMax !== undefined) {
+            query.rentalCost.$lte = filters.rentalCostMax;
+        }
     }
     if (filters.garage !== undefined) {
         query.garage = filters.garage;
     }
     if (filters.petPolicy) {
+        // 确保查询条件与数据库中的数据一致，这里直接使用传入的宠物政策作为查询条件
         query.petPolicy = filters.petPolicy;
     }
 
