9d3ff21d3e48deb6eaf154867a5e93a323c6445e
YuqiStevens <ydong28@stevens.edu>
2024-05-06 20:33:26 -0400

    finished project - extend seed.js

--- test/seed.js
+++ test/seed.js
@@ -1,10 +1,10 @@
-import { addComment, addCommentToReview } from "../data/commentsForReviews";
-import { addHousing } from "../data/housing";
-import { addUser } from "../data/users";
+import { addComment, addCommentToReview } from "../data/commentsForReviews.js";
+import { addHousing, addReviewIdToHousing } from "../data/housing.js";
+import { updateAverageRating} from "../data/reviewsForHousing.js";
+import { ObjectId } from "mongodb";
+import { addUser } from "../data/users.js";
 
-
-
-//--------------------admin1-----------------------
+//--------------------admin1 and user1-----------------------
 let user1_user = await addUser(
     'Xiong',
     'Chen',
@@ -84,11 +84,14 @@ let housingInput2 = {
     homeType: "Apartment",
     amenities: "Infinity Pool, Ocean View Terrace",
     images: "https://s3-media0.fl.yelpcdn.com/bphoto/E53vaJMIjOBKXyPOdQK59Q/o.jpg",
-    rentalCostMin: "5000",
+    rentalCostMin: "1000",
     rentalCostMax: "10000",
-    bedrooms: "5",
-    bathrooms: "4",
-    petPolicy: "Allowed with deposit",
+    studios: "10",
+    oneBed: "20",
+    towBed: "20",
+    threeBed: "15",
+    fourBed: "10",
+    petPolicy: "Allowed",
     garage: "true",
     location: {
         latitude: "40.74622067597064",
@@ -137,4 +140,139 @@ let newComment2 = {
 newComment2 = await addComment(newComment2);
 await addCommentToReview(review2._id.toString(), newComment2);
 
-//--------------------admin2-----------------------
+//--------------------admin2 and user2-----------------------
+
+let user2_user = await addUser(
+    'Yuqi',
+    'Dong',
+    'user2@user.com',
+    'Ydongdong28@',
+    'user',
+    'Jersey City',
+    'NJ',
+    'US',
+    '22',
+    'Master',
+    'Computer Science'
+);
+
+let user2_admin = await addUser(
+    'Yuqi',
+    'Dong',
+    'user2@admin.com',
+    'Ydongdong28@',
+    'admin',
+    'Jersey City',
+    'NJ',
+    'US',
+    '22',
+    'Master',
+    'Computer Science'
+);
+
+let user2_id_user= user2_user.user_id;
+let user2_id_admin= user2_admin.user_id;
+
+let housingInput3 = {
+    address: "270 Tenth St",
+    city: "Jersey City",
+    state: "NJ",
+    zipCode: "07302",
+    homeType: "Apartment",
+    amenities: "Gym",
+    images: "https://s3-media0.fl.yelpcdn.com/bphoto/cILKlysZaGvA4UGgpVqsRQ/o.jpg",
+    rentalCostMin: "1000",
+    rentalCostMax: "5000",
+    studios: "2",
+    oneBed: "4",
+    towBed: "6",
+    threeBed: "8",
+    fourBed: "10",
+    petPolicy: "Allowed",
+    garage: "true",
+    location: {
+        latitude: "40.72962951134248",
+        longitude: "-74.04558213013271"
+    },
+    rating: "2"
+};
+
+let housingId3 = await addHousing(housingInput3);
+
+let review3 = {
+    houseId: housingId3,
+    userId: user2_id_user,
+    rating: 3,
+    title: 'Fair place but really expensive',
+    body: 'Not bad, enough quite to live',
+    images: ['https://s3-media0.fl.yelpcdn.com/bphoto/r4YqKCNI70O0NyVjqY2zJA/o.jpg'],
+    helpfulCounts: 0,
+    comments: []
+};
+
+await addReviewIdToHousing(housingId3, review3._id.toString());
+await updateAverageRating(housingId3);
+
+let housingInput4 = {
+    address: "180 River Dr",
+    city: "Jersey City",
+    state: "NJ",
+    zipCode: "07310",
+    homeType: "House",
+    amenities: "Ocean View Terrace",
+    images: "https://s3-media0.fl.yelpcdn.com/bphoto/ckuiBGVWs83WDiOhmvz8qQ/o.jpg",
+    rentalCostMin: "1000",
+    rentalCostMax: "5000",
+    studios: "20",
+    oneBed: "14",
+    towBed: "16",
+    threeBed: "18",
+    fourBed: "10",
+    petPolicy: "Allowed",
+    garage: "true",
+    location: {
+        latitude: "40.732446643641055",
+        longitude: "-74.0314129085959"
+    },
+    rating: "5"
+};
+
+let housingId4 = await addHousing(housingInput4);
+
+let review4 = {
+    houseId: housingId4,
+    userId: user2_id_admin,
+    rating: 5,
+    title: 'Fantastic Apartment Near Hudson River',
+    body: 'The views from this home are simply breathtaking. Can\'t wait to come back!',
+    images: ['https://s3-media0.fl.yelpcdn.com/bphoto/CtoimFAR-Qsv0HH0VE5qAg/o.jpg'],
+    helpfulCounts: 0,
+    comments: []
+};
+
+await addReviewIdToHousing(housingId4, review4._id.toString());
+await updateAverageRating(housingId4);
+
+let newComment3 = {
+    reviewId: new ObjectId(review3._id.toString()),
+    userId: new ObjectId(user2_id_user),
+    firstName: user1_user.firstName,
+    lastName: user1_user.lastName,
+    comment: "Really good place!!!",
+    createdAt: new Date()
+};
+
+newComment3 = await addComment(newComment3);
+await addCommentToReview(review3._id.toString(), newComment3);
+
+let newComment4 = {
+    reviewId: new ObjectId(review4._id.toString()),
+    userId: new ObjectId(user2_id_admin),
+    firstName: user1_admin.firstName,
+    lastName: user1_admin.lastName,
+    comment: "Really fantastic place!!!",
+    createdAt: new Date()
+};
+
+newComment4 = await addComment(newComment4);
+await addCommentToReview(review4._id.toString(), newComment4);
